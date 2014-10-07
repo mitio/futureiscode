a7e03d5025363d78206f1492ee66ce79ce0e0962
Dimitar Dimitrov <me@ddimitrov.name>
2014-10-07 12:41:14 +0300

    Add buttons to join as a school or a coach

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,11 @@
+$(function () {
+  $(document).on('click', '.smooth-scroll', function (e) {
+    e.preventDefault();
+    var targetSelector = $(this).attr('href');
+    var $target = $(targetSelector);
+    $('html,body').animate({
+      scrollTop: $target.offset().top
+    }, 1000);
+  });
+});
 
