222d73ca8b5bb0707de7273d6c2add11cbec0680
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-01 17:01:40 +0300

    Fix a copy-paste error in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if direction == :up
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
