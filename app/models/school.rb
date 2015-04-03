69ae4901e6c85a5d671de6f432b0416ba396b9e0
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-03 15:50:41 +0300

    Don’t validate email for presence
    Devise already does that.

--- app/models/school.rb
+++ app/models/school.rb
@@ -9,7 +9,7 @@ class School < ActiveRecord::Base
   validates :town_id, presence: true
   validates :address, presence: true
   validates :contact_name, presence: true
-  validates :email, presence: true, email: true
+  validates :email, email: true, allow_blank: true
 
   serialize :details
 
