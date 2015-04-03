c56b3ca348fd91fe48a8845dacfb7820eaa9520f
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-03 13:32:39 +0300

    A separate contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  resources :contacts, only: :index
+
   devise_for :schools
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'home#index'
 end
