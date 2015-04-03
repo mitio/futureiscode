f98698847fa2fb3f62b3d12fa42192fb034b5f73
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-03 17:45:21 +0300

    Custom admin index page for schools

--- app/admin/school.rb
+++ app/admin/school.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register School do
   remove_filter :town
+
+  index do
+    selectable_column
+    id_column
+    column 'Име', :name
+    column 'Град', :town do |row|
+      auto_link row.town, row.town.name
+    end
+    column 'Акт.?', :confirmed_participation
+    column 'Контакт', :contact_name
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
