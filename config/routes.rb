31dd0c1f95332edd4f791f95443f6d4902d7db4b
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-04 19:38:04 +0300

    Redirect schools to their profile after login/profile update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   resources :contacts, only: :index
 
+  as :school do
+    get 'schools/profile', to: 'devise/registrations#edit', as: :school_root
+  end
+
   devise_for :schools
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
