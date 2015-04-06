0d396fc63d22cd3d873c3e708c26b226fd6d1d23
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-06 12:39:34 +0300

    Working edit/create admin UI for schools

--- app/admin/school.rb
+++ app/admin/school.rb
@@ -1,4 +1,9 @@
 ActiveAdmin.register School do
+  permit_params :name, :town_id, :address, :contact_name, :phone, :email,
+                :confirmed_participation, :visit_dates, {disciplines: []},
+                {available_equipment: []}, :meetup_options, :remarks,
+                :password, :password_confirmation
+
   remove_filter :town
 
   index do
@@ -13,5 +18,27 @@ ActiveAdmin.register School do
     column :created_at
     column :updated_at
     actions
+  end
+
+  form do |f|
+    f.inputs 'School Details' do
+      f.input :name
+      f.input :town, collection: Town.order('name ASC').includes(municipality: :state)
+      f.input :address
+      f.input :contact_name
+      f.input :phone
+      f.input :email, hint: 'При промяна, ще бъде изпратено писмо на този адрес за потвърждение.'
+      if f.object.new_record?
+        f.input :password
+        f.input :password_confirmation
+      end
+      f.input :confirmed_participation
+      f.input :visit_dates, as: :text
+      f.input :disciplines, as: :check_boxes, collection: Options.for('disciplines').map { |id, label| [label, id.to_s] }
+      f.input :available_equipment, as: :check_boxes, collection: Options.for('available_equipment').map { |id, label| [label, id.to_s] }
+      f.input :meetup_options, as: :radio, collection: Options.for('meetup_options').map { |id, label| [label, id.to_s] }
+      f.input :remarks, as: :text
+    end
+    f.actions
   end
 end
