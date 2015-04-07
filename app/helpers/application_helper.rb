2007fa2cf9c9f55c1e6b141756448c59b5bcb68d
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-07 13:34:25 +0300

    Add info about the organizing company to events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,23 @@ module ApplicationHelper
   def markup(text)
     Markup.format text
   end
+
+  def link_to_company(company)
+    if company.is_a? Company
+      name_or_logo =
+        if company.logo.present?
+          image_tag(company.logo.url(:small), alt: company.name, title: company.name)
+        else
+          company.name
+        end
+
+      if company.url.present?
+        link_to name_or_logo, company.url, rel: 'nofollow', target: '_blank', class: 'company'
+      else
+        content_tag :span, name_or_logo, class: 'company'
+      end
+    elsif company
+      content_tag :span, company, class: 'company other'
+    end
+  end
 end
