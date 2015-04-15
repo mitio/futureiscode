de4a215054b7f05e1d2bd377641321dee5ae71b1
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-15 03:35:31 +0300

    Show a participating schools counter on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @companies = Company.where(pledged_support: true)
+    @participating_schools_count = School.participating.size
   end
 end
