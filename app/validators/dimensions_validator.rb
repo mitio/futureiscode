781333a1ce850756fd1f946fb8cabcef9511c664
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-15 12:21:57 +0300

    Fix duplicated validation error message key
    The “too_short” key was conflicting with the same one from Devise and
    was causing “missing interpolation argument” errors during signup when
    the password was too short (less than 8 chars).

--- app/validators/dimensions_validator.rb
+++ app/validators/dimensions_validator.rb
@@ -5,11 +5,11 @@ class DimensionsValidator < ActiveModel::EachValidator
     dimensions = Paperclip::Geometry.from_file(value.queued_for_write[:original].path)
 
     if options[:min_width] && dimensions.width < options[:min_width]
-      record.errors.add attribute, :too_narrow, options.slice(:min_width)
+      record.errors.add attribute, :image_too_narrow, options.slice(:min_width)
     end
 
     if options[:min_height] && dimensions.height < options[:min_height]
-      record.errors.add attribute, :too_short, options.slice(:min_width)
+      record.errors.add attribute, :image_too_short, options.slice(:min_width)
     end
   end
 end
