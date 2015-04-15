8f0a1fbb093ab943598fe513640de179a789a33c
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-15 12:23:56 +0300

    Show the current width/height in the “image too small” error

--- app/validators/dimensions_validator.rb
+++ app/validators/dimensions_validator.rb
@@ -3,13 +3,15 @@ class DimensionsValidator < ActiveModel::EachValidator
     return unless value.queued_for_write[:original]
 
     dimensions = Paperclip::Geometry.from_file(value.queued_for_write[:original].path)
+    width      = dimensions.width.round
+    height     = dimensions.height.round
 
-    if options[:min_width] && dimensions.width < options[:min_width]
-      record.errors.add attribute, :image_too_narrow, options.slice(:min_width)
+    if options[:min_width] && width < options[:min_width]
+      record.errors.add attribute, :image_too_narrow, {current_width: width}.merge(options.slice(:min_width))
     end
 
-    if options[:min_height] && dimensions.height < options[:min_height]
-      record.errors.add attribute, :image_too_short, options.slice(:min_width)
+    if options[:min_height] && height < options[:min_height]
+      record.errors.add attribute, :image_too_short, {current_height: height}.merge(options.slice(:min_width))
     end
   end
 end
