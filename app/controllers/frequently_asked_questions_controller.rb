dc0a741120c26caf53b0c8002f54a0e85209fde1
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-21 14:52:12 +0300

    Extract FAQs into separate partials
    This is so we can provide a contents section on the FAQ page with
    links to each of the questions, plus a link to go back to the contents.

--- app/controllers/frequently_asked_questions_controller.rb
+++ app/controllers/frequently_asked_questions_controller.rb
@@ -1,4 +1,22 @@
 class FrequentlyAskedQuestionsController < ApplicationController
   def index
+    @questions = {
+      about:            'Какво е Бъдещето е код?',
+      non_commercial:   'Комерсиална инициатива ли е?',
+      payment:          'Ще бъда ли възмезден като лектор?',
+      who:              'Кой го организира?',
+      motivation:       'Мотивация',
+      history:          'История',
+      mechanism:        'Механизъм',
+      logistics:        'Организация и логистика',
+      content:          'Съдържание на презентациите',
+      meetups:          'Сбирки след часовете',
+      followup:         'Последващи дейности',
+      schedule:         'График и времетраене',
+      join:             'Как да се включите',
+      spread_the_word:  'Популяризация на събитието',
+      opensource:       'Отворен код',
+      contacts:         'Контакти',
+    }
   end
 end
