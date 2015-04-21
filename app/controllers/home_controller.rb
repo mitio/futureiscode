3cd34f2b4327f042f62a311e455f691f647554db
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-21 16:04:22 +0300

    Add an events counter on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @companies = Company.where(pledged_support: true)
     @participating_schools_count = School.participating.size
+    @events = Event.approved
   end
 end
