7a81b9551c70e51cbcd02f49d7b7db7668c1a7c1
Dimitar Dimitrov <me@ddimitrov.name>
2015-04-23 14:49:51 +0300

    Show more counts in the admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,19 +3,37 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do
-    div style: 'margin-bottom: 3em;' do
-      stats = {
-        'Активни и участващи училища' => School.participating.up_to_date.count,
-        'Всички участващи училища'    => School.participating.count,
-        'Остарели участващи училища'  => School.participating.outdated.count,
-        'Отказали се училища'         => School.not_participating.count,
-        'Всички училища'              => School.count,
-        'Регистрирани лектори'        => Speaker.count,
-        'Събития'                     => Event.count,
-      }
+    columns do
+      column do
+        panel 'Училища' do
+          stats = {
+            'Активни и участващи училища' => School.participating.up_to_date.count,
+            'Всички участващи училища'    => School.participating.count,
+            'Остарели участващи училища'  => School.participating.outdated.count,
+            'Отказали се училища'         => School.not_participating.count,
+            'Всички училища'              => School.count,
+          }
 
-      stats.each do |label, value|
-        h2 "#{label}: <strong>#{value}</strong>".html_safe
+          stats.each do |label, value|
+            h2 "#{label}: <strong>#{value}</strong>".html_safe
+          end
+        end
+      end
+
+      column do
+        panel 'Лектори и събития' do
+          stats = {
+            'Регистрирани лектори' => Speaker.count,
+            'Фирми'                => Company.count,
+            'Одобрени събития'     => Event.approved.count,
+            'Чакащи събития'       => Event.pending.count,
+            'Всички събития'       => Event.count,
+          }
+
+          stats.each do |label, value|
+            h2 "#{label}: <strong>#{value}</strong>".html_safe
+          end
+        end
       end
     end
 
