02a7467435ffe744d2422f279f7ab44b242ef273
Dimitar Dimitrov <me@ddimitrov.name>
2022-11-27 19:12:56 +0200

    Clean up commented out code from the deploy setup

--- Capfile
+++ Capfile
@@ -7,8 +7,6 @@ require "capistrano/rbenv"
 require "capistrano/bundler"
 require "capistrano/rails/assets"
 require "capistrano/rails/migrations"
-# require "capistrano/passenger"
-# require 'capistrano/dotenv'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,3 @@ set :repo_url, 'git@github.com:mitio/futureiscode.git'
 
 append :linked_files, 'config/database.yml', '.env'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'public/system', 'vendor', 'storage'
-
-# namespace :deploy do
-#   task :restart, roles: :app, except: {no_release: true} do
-#     run "touch #{current_path}/tmp/restart.txt"
-#   end
-# end
