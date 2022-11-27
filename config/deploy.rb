2c27682dfa8dcf511decdfeca339325057251bf7
Dimitar Dimitrov <me@ddimitrov.name>
2022-11-27 23:47:56 +0200

    Ensure the app is restarted after the deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,3 +5,11 @@ set :repo_url, 'git@github.com:mitio/futureiscode.git'
 
 append :linked_files, 'config/database.yml', '.env'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'public/system', 'vendor', 'storage'
+
+task :restart_passenger do
+  on roles(:web) do
+    execute :touch, "#{current_path}/tmp/restart.txt"
+  end
+end
+
+after 'deploy:published', 'restart_passenger'
